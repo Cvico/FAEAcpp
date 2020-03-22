da366ece518e57d91bd06e42235f33c89b530e5e
cvico <cvicovillalba1998@gmail.com>
2020-03-22 19:38:33 +0100

    Created header and implemented its libraries

---
+++ AnalysisFAEA.h
@@ -0,0 +1,36 @@
+//======================================================================
+//
+//      This class has been made by me because I'm bored and there is
+//      a pandemic worldwide problem :D
+//
+//======================================================================
+
+#ifndef Xmass_h
+#define Xmass_h
+
+//================= ROOT includes =================
+#include "TH2.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TVectorF.h"
+#include "TGraphAsymmErrors.h"
+#include "TString.h"
+#include "TLegend.h"
+#include "TPave.h"
+#include "TPaveText.h"
+#include "TSystem.h"
+#include "TMath.h"
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+//Header file for vector storage
+#include <TClonesArray.h>
+
+//================= C++ includes =================
+#include <vector>
+#include <iostream>
+
+class AnalysisFAEA {
+
+}
