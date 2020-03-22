29c4d91337d5c811fb2bb400cd4ce5d27e51d52e
cvico <cvicovillalba1998@gmail.com>
2020-03-22 23:27:12 +0100

    Loop method implemented in AnalysisFAEA.C

---
+++ AnalysisFAEA.C
@@ -0,0 +1,16 @@
+//========================================
+//Method implementation used for the
+//analysis.
+//=========================================
+
+#define AnalysisFAEA_cxx
+#include "AnalysisFAEA.h"
+
+
+void AnalysisFAEA::Loop(){
+  //Main method for AnalysisFAEA
+  //First we set the path where to read the rootfiles
+  TString Datapath = "../files/"
+  
+}
+
