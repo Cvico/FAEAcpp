390e788fb332b59bf4e293302924ec415635f410
cvico <cvicovillalba1998@gmail.com>
2020-03-22 21:26:05 +0100

    Branches establishing completed

--- AnalysisFAEA.h
+++ AnalysisFAEA.h
@@ -32,7 +32,7 @@
 #include <iostream>
 
 class AnalysisFAEA {
-
+  
  public:
   int			NJet;			     
   float			Jet_Px;			     
@@ -90,57 +90,71 @@ class AnalysisFAEA {
 
   //List of branches
 
-	 
-  TBranch		NJet;			    
-  TBranch		Jet_Px;			    
-  TBranch		Jet_Py;			    
-  TBranch		Jet_Pz;			    
-  TBranch		Jet_E;			    
-  TBranch		Jet_btag;		    
-  TBranch	 	Jet_ID;			    
-  TBranch		NMuon;			    
-  TBranch		Muon_Px;		    
-  TBranch		Muon_Py;		    
-  TBranch		Muon_Pz;		    
-  TBranch		Muon_E;			    
-  TBranch		Muon_Charge;		    
-  TBranch	 	Muon_Iso;		    
-  TBranch		NElectron;		    
-  TBranch		Electron_Px;		    
-  TBranch		Electron_Py;		    
-  TBranch		Electron_Pz;		    
-  TBranch		Electron_E;		    
-  TBranch		Electron_Charge;	    
-  TBranch	 	Electron_Iso;		    
-  TBranch		NPhoton;		    
-  TBranch		Photon_Px;		    
-  TBranch		Photon_Py;		    
-  TBranch		Photon_Pz;		    
-  TBranch		Photon_E;		    
-  TBranch		Photon_Charge;		    
-  TBranch		Photon_Iso;		    
-  TBranch			MET_px;			    
-  TBranch			Met_py;			    
-  TBranch			MChadronicBottom_px;	    
-  TBranch			MChadronicBottom_py;	    
-  TBranch			MChadronicBottom_pz;	    
-  TBranch			MCleptonicBottom_px;	    
-  TBranch			MCleptonicBottom_py;	    
-  TBranch			MCleptonicBottom_pz;	    
-  TBranch			MChadronicWDecayQuark_px;   
-  TBranch			MChadronicWDecayQuark_py;   
-  TBranch			MChadronicWDecayQuark_pz;   
-  TBranch			MChadronicWDecayQuarkBar_px;
-  TBranch			MChadronicWDecayQuarkBar_py;
-  TBranch			MChadronicWDecayQuarkBar_pz;
-  TBranch			MClepton_px;		    
-  TBranch			MClepton_py;		    
-  TBranch			MClepton_pz;		    
-  TBranch			MClepton_PDGid;		    
-  TBranch			MCneutrino_px;		    
-  TBranch			MCneutrino_py;		    
-  TBranch			MCneutrino_pz;		    
-  TBranch			NPrimaryVertices;	    
-  TBranch			triggerIsoMu24;		    
-  TBranch			EventWeight;                
-}
+
+  TBranch		NJet;                                        //!			    
+  TBranch		Jet_Px;			    		     //!
+  TBranch		Jet_Py;			    		     //!
+  TBranch		Jet_Pz;			    		     //!
+  TBranch		Jet_E;			    		     //!
+  TBranch		Jet_btag;		    		     //!
+  TBranch	 	Jet_ID;			    		     //!
+  TBranch		NMuon;			    		     //!
+  TBranch		Muon_Px;		    		     //!
+  TBranch		Muon_Py;		    		     //!
+  TBranch		Muon_Pz;		    		     //!
+  TBranch		Muon_E;			    		     //!
+  TBranch		Muon_Charge;		    		     //!
+  TBranch	 	Muon_Iso;		    		     //!
+  TBranch		NElectron;		    		     //!
+  TBranch		Electron_Px;		    		     //!
+  TBranch		Electron_Py;		    		     //!
+  TBranch		Electron_Pz;		    		     //!
+  TBranch		Electron_E;		    		     //!
+  TBranch		Electron_Charge;	    		     //!
+  TBranch	 	Electron_Iso;		    		     //!
+  TBranch		NPhoton;		    		     //!
+  TBranch		Photon_Px;		    		     //!
+  TBranch		Photon_Py;		    		     //!
+  TBranch		Photon_Pz;		    		     //!
+  TBranch		Photon_E;		    		     //!
+  TBranch		Photon_Charge;		    		     //!
+  TBranch		Photon_Iso;		    		     //!
+  TBranch		        MET_px;			    	     //!
+  TBranch			Met_py;			    	     //!
+  TBranch			MChadronicBottom_px;	    	     //!
+  TBranch			MChadronicBottom_py;	    	     //!
+  TBranch			MChadronicBottom_pz;	    	     //!
+  TBranch			MCleptonicBottom_px;	    	     //!
+  TBranch			MCleptonicBottom_py;	    	     //!
+  TBranch			MCleptonicBottom_pz;	    	     //!
+  TBranch			MChadronicWDecayQuark_px;   	     //!
+  TBranch			MChadronicWDecayQuark_py;   	     //!
+  TBranch			MChadronicWDecayQuark_pz;   	     //!
+  TBranch			MChadronicWDecayQuarkBar_px;	     //!
+  TBranch			MChadronicWDecayQuarkBar_py;	     //!
+  TBranch			MChadronicWDecayQuarkBar_pz;	     //!
+  TBranch			MClepton_px;		    	     //!
+  TBranch			MClepton_py;		    	     //!
+  TBranch			MClepton_pz;		    	     //!
+  TBranch			MClepton_PDGid;		    	     //!
+  TBranch			MCneutrino_px;		    	     //!
+  TBranch			MCneutrino_py;		    	     //!
+  TBranch			MCneutrino_pz;		    	     //!
+  TBranch			NPrimaryVertices;	    	     //!
+  TBranch			triggerIsoMu24;		    	     //!
+  TBranch			EventWeight;                         //!
+
+  //================================ Method Declaration
+  //Constructor
+  AnalysisFAEA(TTree *tree = 0);
+  //Destructor
+  ~AnalysisFAEA();
+  int cut(int entry);
+  int GetEntry(int entry);
+  int LoadTree(int entry);
+  void Init(TTree *tree);
+  void Loop(float cut);
+  Bool_t Notify();
+  void Show(int entry = -1);
+};
+#endif
