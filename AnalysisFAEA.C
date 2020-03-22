65b7a14683f12dfa2215d36072bda784183ea07b
cvico <cvicovillalba1998@gmail.com>
2020-03-22 23:42:50 +0100

    Loop method implemented in .C file

--- AnalysisFAEA.C
+++ AnalysisFAEA.C
@@ -11,6 +11,11 @@ void AnalysisFAEA::Loop(){
   //Main method for AnalysisFAEA
   //First we set the path where to read the rootfiles
   TString Datapath = "../files/"
+
+    //for(int i = 0; i < Backgrounds.length(); i++){
+    // std::cout << "Background: " << Backgrounds.at(i) << std::endl;
+    //}
   
+    
 }
 
