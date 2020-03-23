2993113ef64aeea4c2af2c807565e5bb350621ba
cvico <cvicovillalba1998@gmail.com>
2020-03-23 23:11:39 +0100

    Setting fill methods

--- AnalysisFAEA.C
+++ AnalysisFAEA.C
@@ -6,21 +6,19 @@
 #define AnalysisFAEA_cxx
 #include "AnalysisFAEA.h"
 
-
+/*
 void AnalysisFAEA::Loop(std::vector<TString> VectorSamples){
   for (int i = 0; VectorSamples.size(); i++){
     Loop(VectorSamples.at(i));
   }
   return
 }
+*/
 void AnalysisFAEA::Loop(TString sample){
-
+  
   //First we need to create the histograms for the analysis
   InitHistos();
-
-
-  TString Datapath = "../files/";
-    
+  std::cout << "Histos initialized" << std::endl;
 }
 
 
@@ -32,11 +30,11 @@ void AnalysisFAEA::InitHistos(){
   TH1F* DiMuonMass = new TH1F("_DiMuonMass", "", 20, 0, 200);
   TH1F* NJet = new TH1F("_NJet", "", 10, 0, 10);
   TH1F* MET = new TH1F("_MET", "", 30, 0, 200);
-  TH1F* MuonEta = new TH1F("_MuonEta", 20, -3, 3);
+  TH1F* MuonEta = new TH1F("_MuonEta","", 20, -3, 3);
   
 }
 
-
+/*
 void AnalysisFAEA::FillHistograms(){
 
   MuonPt->Fill(MuonPt, weight);
@@ -46,3 +44,4 @@ void AnalysisFAEA::FillHistograms(){
   MuonEta->Fill(MuonEta, weight);
 
 }
+*/
