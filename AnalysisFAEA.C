52b11664fa2ea78f496da4220d606e711b1030ff
cvico <cvicovillalba1998@gmail.com>
2020-03-30 21:46:58 +0200

    Changed Fill method

--- AnalysisFAEA.C
+++ AnalysisFAEA.C
@@ -23,26 +23,22 @@ void AnalysisFAEA::Loop(TString sample){
     
     //Iterate over all events
     int nevents = Tree->GetEntries();
-    std::cout << nevents << std::endl;
+    std::cout << "Looping over " << nevents << " events." << std::endl;
     for (int event = 0; event<nevents; event++){
         GetEntry(event);
-        if(event%1000 == 0){std::cout << "Analizando..." << std::endl;}
-        //TLorentzVector *Muon1 = new TLorentzVector();
-        //Muon1->SetPxPyPzE(Muon_Px[0], Muon_Py[0], Muon_Pz[0], Muon_E[0]);
-        
-        
-        //MuonPt = float(Muon1->Pt());
-        //MuonEta = float(Muon1->Eta());
-        MuonPt = 1;
-        MuonEta = 1;
+        if(event%5000 == 0){std::cout << (float)event/nevents*100 <<"% analysed..." << std::endl;}
+        TLorentzVector *Muon1 = new TLorentzVector();
+        Muon1->SetPxPyPzE(Muon_Px[0], Muon_Py[0], Muon_Pz[0], Muon_E[0]);
+        MuonPt = float(Muon1->Pt());
+        MuonEta = float(Muon1->Eta());
         MET = (MET_px * MET_px + MET_py * MET_py);
-        
+        weight = EventWeight;
+        FillHistograms();
         //delete Muon1;
     }
     
     fOut->Close();
     delete fOut;
-    std::cout << "File output deleted" << std::endl;
     return;
     
 }
@@ -53,21 +49,17 @@ void AnalysisFAEA::InitHistos(){
   //Some debug output
   std::cout << "[INFO]Creating histograms for the analysis..." << std::endl;
   TH1F* h_MuonPt = new TH1F("h_MuonPt", "", 20, 0, 200);
-  //TH1F* h_DiMuonMass = new TH1F("h_DiMuonMass", "", 20, 0, 200);
   TH1F* h_NJet = new TH1F("h_NJet", "", 10, 0, 10);
   TH1F* h_MET = new TH1F("h_MET", "", 30, 0, 200);
   TH1F* h_MuonEta = new TH1F("h_MuonEta","", 20, -3, 3);
   
+  h_MuonPt->SetDirectory(0);
+  return;
 }
 
 
 
 void AnalysisFAEA::FillHistograms(){
-
-  ((TH1F*)gROOT->FindObject("h_MuonPt"))->Fill(MuonPt, weight);
-  //((TH1F*)gROOT->FindObject("h_DiMuonMass"))->Fill(DiMuonMass, weight);
-  ((TH1F*)gROOT->FindObject("h_NJet"))->Fill(NJet, weight);
-  ((TH1F*)gROOT->FindObject("h_MET"))->Fill(MET, weight);
-  ((TH1F*)gROOT->FindObject("h_MuonEta"))->Fill(MuonEta, weight);
+        
 
 }
